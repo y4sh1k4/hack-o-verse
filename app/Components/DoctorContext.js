dcbff74adc9d91ed7491b32ec2430f26413aada3
sh4d0wy <sakshambhugra8@gmail.com>
2024-04-20 15:16:53 +0530

    Context updated

--- app/Components/DoctorContext.js
+++ app/Components/DoctorContext.js
@@ -3,7 +3,9 @@ import React, { createContext, useState } from 'react'
 export const doctorContext = createContext();
 
 export const DoctorContext = ({children}) => {
-    const [doctor,setDoctor] = useState({}); 
+    const [doctor,setDoctor] = useState({
+      name:"Yashika"
+    }); 
   return (
     <doctorContext.Provider value={{doctor,setDoctor}}>
         {children}
--- app/Components/PatientContext.js
+++ app/Components/PatientContext.js
@@ -3,7 +3,9 @@ import React, { createContext, useState } from 'react'
 export const patientContext = createContext();
 
 export const PatientContext = ({children}) => {
-    const [patient,setPatient] = useState({}); 
+    const [patient,setPatient] = useState({
+      name:"Saksham Bhugra"
+    }); 
   return (
     <patientContext.Provider value={{patient,setPatient}}>
         {children}
